fc327e0d4448e0cb412963bff05f9b68a8b9708e
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-07-12 21:32:08 +0530

    Modify businessDetails schema

--- models/businessDetails.js
+++ models/businessDetails.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 export const businessSchema = new mongoose.Schema({
   businessName: { type: String, required: true },
   gstin: { type: String, required: true },
-  yearsInOperation: { type: Number, required: true },
+  years: { type: String, required: true },
   businessAddress: { type: String, required: true },
   landmark: { type: String },
-  pinCode: { type: String, required: true },
-  operationalCities: { type: [String], required: true },
+  pinCode: { type: Number, required: true },
+  cities: { type: [String], required: true },
 });
