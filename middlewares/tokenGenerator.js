5a8138ae89bc83d57f48514172f636a6eb80504f
QuarterColon <QuarterColon@users.noreply.github.com>
2024-08-08 14:45:18 +0000

    chore: apply formatting changes [skip vercel]

--- middlewares/tokenGenerator.js
+++ middlewares/tokenGenerator.js
@@ -1,8 +1,8 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 let userId = 1;
-let password = '123456';
+let password = "123456";
 
-const secret = "9UbH34lyXTQOqHg+zP8YHbccifDK0qKes9y1Dq36"
+const secret = "9UbH34lyXTQOqHg+zP8YHbccifDK0qKes9y1Dq36";
 
 console.log(jwt.sign({ userId, password }, secret));
