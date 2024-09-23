5eb1239c0048fd853195989f33b74269c452f29e
bhanupratapsingh21 <bhanupss137@gmail.com>
2024-09-23 21:57:24 +0530

    Business details added new fields

--- models/businessDetails.js
+++ models/businessDetails.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 export const businessSchema = new mongoose.Schema({
   businessName: { type: String, required: true },
+  category: { type: String, required: true },
   gstin: { type: String, required: true },
+  teamsize: { type: String, required: true },
   years: { type: String, required: true },
   businessAddress: { type: String, required: true },
   landmark: { type: String },
   pinCode: { type: Number, required: true },
   cities: { type: [String], required: true },
+  annualrevenue: { type: String, required: true },
 });
