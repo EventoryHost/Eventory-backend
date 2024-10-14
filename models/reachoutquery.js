835ee87c68f33b11c4c86cce78b280cfe78bb763
bhanupratapsingh21 <bhanupss137@gmail.com>
2024-10-14 16:24:17 +0530

    Created reachout Form Api at /api/query/create-reachout-query path

---
+++ models/reachoutquery.js
@@ -0,0 +1,21 @@
+import { Schema as _Schema, model } from "mongoose";
+const Schema = _Schema;
+
+const reactoutquerySchema = new Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    mobileno: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+});
+
+const reactoutQuery = model("reactoutQuery", reactoutquerySchema);
+
+export default reactoutQuery;
--- routes/queryRoutes.js
+++ routes/queryRoutes.js
@@ -3,5 +3,7 @@ const queryRoutes = Router();
 import queryController from "../controllers/queryController.js";
 
 queryRoutes.post("/create-query", queryController.createQuery);
+queryRoutes.post("/create-reachout-query", queryController.createreachoutQuery);
+
 
 export default queryRoutes;
