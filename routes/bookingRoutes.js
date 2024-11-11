83ee6710e5abdcf57fc26df2c8827a2cac8d6256
harshita <pathakharshita06@gmail.com>
2024-11-11 20:17:05 +0530

    calendar

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getBooking, updateSchedule } from "../controllers/bookingsController.js";
+import { deleteSchedule, getBooking, updateSchedule } from "../controllers/bookingsController.js";
 
 const router = express.Router();
 
 router.get("/getbookings", getBooking);
-router.post("/schedule",updateSchedule)
+router.post("/schedule",updateSchedule);
+router.delete("/schedule/delete",deleteSchedule)
 
 export default router;
