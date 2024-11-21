5bd0d82116696fd281efea438fe07ae1f3598e07
bhanupratapsingh21 <bhanupss137@gmail.com>
2024-11-21 18:36:59 +0530

    added gstapi

---
+++ routes/verificationRoutes.js
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import express from "express";
+const verificationRoutes = express.Router();
+
+import { verifyGSTIN } from "../controllers/verificationController.js";
+
+verificationRoutes.get("/GSTIN/:GSTIN", verifyGSTIN);
+
+
+export default verificationRoutes;
