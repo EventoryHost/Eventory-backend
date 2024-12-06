47981d42baffe99e17e8e08481f73d0b7b08e766
meanmachine889 <ybharadwaj131@gmail.com>
2024-12-06 21:57:19 +0530

    dashboard backend

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -3,6 +3,7 @@ import {
   deleteSchedule,
   getBooking,
   updateSchedule,
+  fetchBooking
 } from "../controllers/bookingsController.js";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/getbookings", getBooking);
 router.post("/schedule", updateSchedule);
 router.delete("/schedule/delete", deleteSchedule);
+router.get("/fetchbookings", fetchBooking);
 
 export default router;
