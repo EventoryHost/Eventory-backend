8cab46dc6497e20ac47247789ec63556ba265cd5
Arnav Jagia <89345926+arnavjagia@users.noreply.github.com>
2024-12-29 21:41:12 +0530

    Bring over updates from bug/84

--- api/index.js
+++ api/index.js
@@ -14,11 +14,8 @@ import queryRoutes from "../routes/queryRoutes.js";
 import { businessDetailsRoutes } from "../routes/reduxRoutes/businessDetails.js";
 import updatePageRoutes from "../routes/updatePageRoutes.js";
 import fileRoutes from "../routes/fileRoutes.js";
-import venueQuotationRoutes from "../routes/venueQuotationRoutes.js";
+import quotationRoutes from "../routes/quotationRoutes.js";
 import verificationRoutes from "../routes/verificationRoutes.js";
-import serviceRouter from "../routes/servicesRoutes.js";
-import venueRouter from "../routes/venueRoutes.js";
-import featuredVendorsRoutes from "../routes/featuredVendorsRoutes.js";
 
 const app = express();
 const port = 4000;
@@ -62,12 +59,9 @@ app.use("/api/query", queryRoutes);
 app.use("/api/email", emailRoutes);
 app.use("/api/about-email", aboutEmailRoutes);
 app.use("/api/files", fileRoutes);
-app.use("/api/quotations", venueQuotationRoutes);
+app.use("/api/quotations", quotationRoutes);
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/verfication", verificationRoutes);
-app.use("/api/service", serviceRouter);
-app.use("/api/venue", venueRouter);
-app.use("/api", featuredVendorsRoutes);
 
 app.get("/", (req, res) => {
   res.status(201).send("Eventory APIs are running...");
