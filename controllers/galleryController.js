cf260c08d81a5a175a56d15bb4f05d737012268b
meanmachine889 <ybharadwaj131@gmail.com>
2024-12-31 10:48:04 +0530

    vd

--- controllers/galleryController.js
+++
@@ -1,5 +0,0 @@
-export const getGallery = async (req, res) => {
-    try {
-        
-    }
-}
--- routes/galleryRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {
-  removeFavourite,
-} from "../controllers/customerController.js";
-const router = express.Router();
-router.get("/:type/:serId", removeFavourite);
-
-export default router;
