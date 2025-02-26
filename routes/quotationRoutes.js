3efb31aca94c3c0cc2a94baf3ed138b15a2f3495
Abhishek6546 <abhishekk11603@gmail.com>
2025-02-26 11:20:31 +0530

    changes

--- routes/quotationRoutes.js
+++ routes/quotationRoutes.js
@@ -74,7 +74,7 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const { vendor_id } = req.query;
- 
+    console.log("id is ",vendor_id)
 
     if (!vendor_id) {
       return res.status(400).json({
