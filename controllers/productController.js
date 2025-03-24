b1fa0b0fa0292ff433e29d44e895d689373c81a6
priyanshubisht10 <priyanshubisht10@users.noreply.github.com>
2025-03-24 06:17:21 +0000

    chore: apply formatting changes [skip vercel]

--- controllers/productController.js
+++ controllers/productController.js
@@ -95,7 +95,6 @@ const searchDecorators = async (query) => {
     filters["themesOffered.themesOffered"] = { $in: query.themes };
   }
 
-
   let decoratorQuery = Decorator.find(filters);
 
   const apiFeatures = new APIFeatures(decoratorQuery, query)
