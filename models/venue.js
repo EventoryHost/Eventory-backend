b22088a75a983232a15dbd2444a0114e2e028c5b
QuarterColon <ankitchakraborty1972@gmail.com>
2025-04-01 12:09:01 +0530

    fixes

--- models/venue.js
+++ models/venue.js
@@ -63,7 +63,7 @@ const venueSchema = new Schema({
   },
 
   featureDetails: {
-    completed: { type: Boolean, default: false }, // Flag for section completion
+    completed: { type: Boolean, default: false },  // Flag for section completion
     catererServices: { type: Boolean, required: true },
     decorServices: { type: Boolean, required: true },
     eventTypes: { type: [String], required: true },
@@ -96,15 +96,9 @@ const venueSchema = new Schema({
     cancellationPolicy: { type: [String] },
     insurancePolicy: { type: [String] },
   },
-  reviews: [
-    {
-      rating: { type: Number, required: true },
-      name: { type: String, required: true },
-      feedback: { type: String, required: true },
-      photos: { type: [String] },
-      date: { type: String, required: true },
-    },
-  ],
+
+  rating: { type: Number, default: 0, min: 0, max: 5 }, // Aggregate rating of all reviews
+
 });
 
 const Venue = model("Venue", venueSchema);
