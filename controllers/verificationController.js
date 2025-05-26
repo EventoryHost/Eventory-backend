6fce05e55a5c704998cc0d907a52b00b26f6bb1c
QuarterColon <QuarterColon@users.noreply.github.com>
2025-05-26 21:30:34 +0000

    chore: apply formatting changes [skip vercel]

--- controllers/verificationController.js
+++ controllers/verificationController.js
@@ -20,7 +20,6 @@ const verifyGSTIN = async (req, res) => {
     const clientId = process.env.CASHFREE_CLIENT_ID;
     const clientSecret = process.env.CASHFREE_CLIENT_SECRET;
     let publicKey = `${process.env.CASHFREE_PUBLIC_KEY}`;
-    
 
     const timestamp = Math.floor(Date.now() / 1000);
 
