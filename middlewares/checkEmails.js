1cfbd8a1a75568d4c890b03e25a3be7f691aa2e6
priyanshubisht10 <priyanshubisht10@users.noreply.github.com>
2025-06-13 16:54:17 +0000

    chore: apply formatting changes [skip vercel]

--- middlewares/checkEmails.js
+++ middlewares/checkEmails.js
@@ -1,16 +1,16 @@
-import findEmails from 'find-emails-in-string';
+import findEmails from "find-emails-in-string";
 
 // Rename the function to match its purpose
 export const checkEmails = (text) => {
-    if (!text || typeof text !== "string") return false;
+  if (!text || typeof text !== "string") return false;
 
-    try {
-        const emailMatches = findEmails(text);
-        const hasEmail = emailMatches.length > 0;
+  try {
+    const emailMatches = findEmails(text);
+    const hasEmail = emailMatches.length > 0;
 
-        return hasEmail;
-    } catch (error) {
-        console.error("Error checking for Emails:", error);
-        return false; // In case of error, let the message through
-    }
+    return hasEmail;
+  } catch (error) {
+    console.error("Error checking for Emails:", error);
+    return false; // In case of error, let the message through
+  }
 };
