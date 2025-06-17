49d99b43863a6a8bfd53f55de1c39542c4edfc6f
Ankit Kumar Chakraborty <ankitchakraborty1972@gmail.com>
2025-06-17 15:53:00 +0530

    Prod update (#141)
    * add: email check
    
    * chore: apply formatting changes
    [skip vercel]
    
    * Create deploy.yml
    
    * Update index.js
    
    * Update deploy.yml
    
    ---------
    
    Co-authored-by: Priyanshu Bisht <priyanshubisht451@gmail.com>
    Co-authored-by: priyanshubisht10 <priyanshubisht10@users.noreply.github.com>

---
+++ middlewares/checkEmails.js
@@ -0,0 +1,16 @@
+import findEmails from "find-emails-in-string";
+
+// Rename the function to match its purpose
+export const checkEmails = (text) => {
+  if (!text || typeof text !== "string") return false;
+
+  try {
+    const emailMatches = findEmails(text);
+    const hasEmail = emailMatches.length > 0;
+
+    return hasEmail;
+  } catch (error) {
+    console.error("Error checking for Emails:", error);
+    return false; // In case of error, let the message through
+  }
+};
