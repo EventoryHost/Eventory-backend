c74dda2bbcb27aaa9706bbf4bb56053e17b0a70a
Prasanna <139959015+Prasannakbhat123@users.noreply.github.com>
2025-06-19 23:55:57 +0530

    Feature/sales (#144)
    * add: webhooks
    
    * fix
    
    * whatsapp
    
    * add: new promo logic
    
    * get promos
    
    * get vendors working
    
    * fix: bug
    
    * fix: bugs
    
    * chore: apply formatting changes
    [skip vercel]
    
    ---------
    
    Co-authored-by: Priyanshu Bisht <priyanshubisht451@gmail.com>
    Co-authored-by: Prasannakbhat123 <prasannabhat@gmail.com>
    Co-authored-by: Prasannakbhat123 <Prasannakbhat123@users.noreply.github.com>

---
+++ models/promo.js
@@ -0,0 +1,33 @@
+import { Schema as _Schema, model } from "mongoose";
+const Schema = _Schema;
+
+const promotionsSchema = new Schema({
+  phoneNumber: { type: String, required: true },
+
+  canSend: {
+    value: { type: Boolean, default: true },
+    updatedAt: { type: Date, default: Date.now },
+  },
+
+  sentBeforeCount: {
+    value: { type: Number, default: 0 },
+    updatedAt: { type: Date, default: Date.now },
+  },
+
+  callRequest: {
+    value: { type: Boolean, default: false },
+    updatedAt: { type: Date, default: null },
+  },
+
+  reqToJoinCommunity: {
+    value: { type: Boolean, default: false },
+    updatedAt: { type: Date, default: null },
+  },
+
+  lastSentDate: { type: Date, default: Date.now }, // last promotion sent
+  vendorName: { type: String, required: true },
+  vendorType: { type: String, required: true },
+});
+
+const Promotion = model("Promotion", promotionsSchema);
+export { Promotion };
