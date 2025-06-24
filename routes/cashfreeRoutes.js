13e59e096d78457b5f49b36ad68ab9cc54add31f
Ankit Kumar Chakraborty <ankitchakraborty1972@gmail.com>
2025-06-25 01:03:32 +0530

    cashfree migrated init (#148)

---
+++ routes/cashfreeRoutes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+const cashfreeRoutes = Router();
+import cashfreeController from "../controllers/cashfree.js";
+
+cashfreeRoutes.post("/create-order", cashfreeController.createOrder);
+cashfreeRoutes.post("/verify-payment", cashfreeController.verifyPayment);
+cashfreeRoutes.post(
+  "/generate-invoice",
+  cashfreeController.generateInvoiceWithDiscount,
+);
+cashfreeRoutes.get("/payment-session/:order_id", cashfreeController.getPaymentSession);
+cashfreeRoutes.post("/webhook", cashfreeController.handleWebhook);
+
+export default cashfreeRoutes;
