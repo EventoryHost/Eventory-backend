bfd6771231b89256a159446f5b76555326a408d8
QuarterColon <ankitchakraborty1972@gmail.com>
2025-06-25 03:14:55 +0530

    auth fail bug fixed

--- controllers/cashfree.js
+++ controllers/cashfree.js
@@ -1,4 +1,4 @@
-import { Cashfree } from "cashfree-pg";
+import { Cashfree, CFEnvironment } from "cashfree-pg";
 import generateInvoice, {
   sendInvoiceWithDiscount,
 } from "../utils/generateInvoice.js";
@@ -14,8 +14,8 @@ import { sendInvoiceToWhatsApp } from "./waController.js";
 const clientId = process.env.CASHFREE_CLIENT_ID_PG;
 const clientSecret = process.env.CASHFREE_CLIENT_SECRET_PG;
 
-var cashfree = process.env.IS_LOCAL === true ? new Cashfree(Cashfree.SANDBOX, `${clientId}`, `${clientSecret}`) :
-  new Cashfree(Cashfree.PRODUCTION, `${clientId}`, `${clientSecret}`);
+const cashfree = process.env.IS_LOCAL === true ? new Cashfree(CFEnvironment.SANDBOX, `${clientId}`, `${clientSecret}`) :
+  new Cashfree(CFEnvironment.PRODUCTION, `${clientId}`, `${clientSecret}`);
 
 
 
