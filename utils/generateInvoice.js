26d3921d7edaa0d199f18b98a577148021e56083
QuarterColon <ankitchakraborty1972@gmail.com>
2025-06-30 23:09:00 +0530

    chages for linux

--- utils/generateInvoice.js
+++ utils/generateInvoice.js
@@ -26,38 +26,32 @@ async function generateInvoice(customer, paymentDetails) {
 
     const subtotal = (paymentDetails.amount) * 0.82;
     const discount = parseInt(paymentDetails.discount).toFixed(2) || 0;
-    console.log("discount", discount);
     const tax = (paymentDetails.amount - paymentDetails.discount) * 0.18;
     let taxSection = "";
     if (customer.businessDetails.pinCode.toString().startsWith("1")) {
       // CGST & SGST for Delhi-based pincodes
       const cgst = tax / 2;
       const sgst = tax / 2;
-      taxSection = `<p>Subtotal: ₹ ${subtotal.toFixed(2)}</p>
-      ${discount !== 0 ? `<p>Discount: -₹ ${discount * 0.82}</p>` : ""}
-        <p>CGST (9%): ₹ ${cgst.toFixed(2)}</p>
-        <p>SGST (9%): ₹ ${sgst.toFixed(2)}</p>
+      taxSection = `<p>Subtotal: \u20B9 ${subtotal.toFixed(2)}</p>
+      ${discount !== 0 ? `<p>Discount: -\u20B9 ${(discount * 0.82).toFixed(2)}</p>` : ""}
+        <p>CGST (9%): \u20B9 ${cgst.toFixed(2)}</p>
+        <p>SGST (9%): \u20B9 ${sgst.toFixed(2)}</p>
         
-        <h3>Total: ₹ ${paymentDetails.amount}</h3>`
+        <h3>Total: \u20B9 ${(paymentDetails.amount - paymentDetails.discount).toFixed(2)}</h3>`
         ;
     } else {
       // IGST for other pincodes
 
       taxSection = `
-      <p>Subtotal: ₹ ${subtotal.toFixed(2)}</p>
-      ${discount !== 0 ? `<p>Discount: -₹ ${discount * 0.82}</p>` : ""}
-      <p>IGST (18%): ₹ ${tax.toFixed(2)}</p>
-      <h3>Total: ₹ ${paymentDetails.amount}</h3>
+      <p>Subtotal: \u20B9 ${subtotal.toFixed(2)}</p>
+      ${discount !== 0 ? `<p>Discount: -\u20B9 ${(discount * 0.82).toFixed(2)}</p>` : ""}
+      <p>IGST (18%): \u20B9 ${tax.toFixed(2)}</p>
+      <h3>Total: \u20B9 ${(paymentDetails.amount - paymentDetails.discount).toFixed(2)}</h3>
 
       `;
     }
 
-    if (discount !== 0) {
-      html = html.replace("{{discountRow}}", `<tr>
-      
-      <td>₹ ${discount * 0.82}</td>
-    </tr>`);
-    }
+
 
     // Replace placeholders with actual data
 
@@ -73,9 +67,8 @@ async function generateInvoice(customer, paymentDetails) {
       "{{customerAddress}}",
       customer.businessDetails.businessAddress,
     );
-    html = html.replace("{{amount}}", paymentDetails.amount);
+    html = html.replace("{{amount}}", (paymentDetails.amount - paymentDetails.discount).toFixed(2));
 
-    if (paymentDetails.discount !== 0) html = html.replace("{{discount}}", paymentDetails.discount * 0.82);
     html = html.replace("{{gstin}}", customer.businessDetails.gstin);
     html = html.replaceAll("{{subtotal}}", subtotal.toFixed(2));
     html = html.replace("{{taxSection}}", taxSection);
