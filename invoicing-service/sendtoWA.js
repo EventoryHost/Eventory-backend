bc3ff8f25c3d67a5624180d18506f6c6d70e07a5
Aman <amanwairagkar@gmail.com>
2025-08-06 00:37:30 +0530

    Both whatsapp message template done

--- invoicing-service/sendtoWA.js
+++ invoicing-service/sendtoWA.js
@@ -1,26 +1,24 @@
 import axios from "axios";
 
-export async function sendInvoiceToWhatsApp(link, mobile) {
-  // WhatsApp Business API configuration
+export async function sendInvoiceToWhatsApp(link, mobile, vendorName = "Vendor") {
   const WHATSAPP_API_URL = `https://graph.facebook.com/v22.0/${process.env.WA_PHONE_NUMBER_ID}/messages`;
 
   const headers = {
     Authorization: `Bearer ${process.env.WA_ACCESS_TOKEN}`,
     "Content-Type": "application/json",
   };
+
   try {
-    // Format invoice message
     const messageResponse = await axios.post(
-      `${WHATSAPP_API_URL}`,
+      WHATSAPP_API_URL,
       {
         messaging_product: "whatsapp",
-        to: `${mobile}`,
+        to: mobile,
         type: "template",
         template: {
-          namespace: "0049ed7f_abf6_48d9_84dc_49ea2de33f57",
-          name: "vendor_receipt_onboarding",
+          name: "vendor_onboarding_message_1_v1",
           language: {
-            code: "en_US",
+            code: "en",
           },
           components: [
             {
@@ -29,23 +27,49 @@ export async function sendInvoiceToWhatsApp(link, mobile) {
                 {
                   type: "document",
                   document: {
-                    link: `${link}`,
-                    filename: "invoice-eventory",
+                    link: link,
+                    filename: "invoice-eventory.pdf", 
                   },
+                },
+              ],
+            },
+            {
+              type: "body",
+              parameters: [
+                {
+                  type: "text",
+                  text: vendorName, 
                 },
               ],
             },
           ],
         },
       },
-      { headers },
+      { headers }
     );
 
-    // Send message via WhatsApp Business API
+    const secondMessageResponse = await axios.post(
+      WHATSAPP_API_URL,
+      {
+        messaging_product: "whatsapp",
+        to: mobile,
+        type: "template",
+        template: {
+          name: "vendor_onboarding_message_2_v1",
+          language: {
+            code: "en",
+          },
+        },
+      },
+      { headers }
+    );
 
-    return messageResponse.data;
+    return {
+      firstMessage: messageResponse.data,
+      secondMessage: secondMessageResponse.data,
+    }
   } catch (error) {
-    console.error("Error sending invoice:", error.message);
+    console.error("Error sending invoice:", error.response?.data || error.message);
     throw error;
   }
 }
