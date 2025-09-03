05f4aa400150ffac17880df99e8a3b9659ef8db3
Aman <amanwairagkar@gmail.com>
2025-09-03 21:01:02 +0530

    Vendor quotataion message added

--- controllers/waController.js
+++ controllers/waController.js
@@ -571,6 +571,49 @@ async function sendCustomerEventBookingMessage(customer_mobile,date,time,venue,l
   }
 }
 
+
+async function sendVendorQuotationMessage(vendor_mobile, vendor_name, quotationLink) {
+  const WHATSAPP_API_URL = `https://graph.facebook.com/v22.0/${process.env.WA_PHONE_NUMBER_ID}/messages`;
+
+  const headers = {
+    Authorization: `Bearer ${process.env.WA_ACCESS_TOKEN}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    const messageResponse = await axios.post(
+      WHATSAPP_API_URL,
+      {
+        messaging_product: "whatsapp",
+        to: `${vendor_mobile}`,
+        type: "template",
+        template: {
+          namespace: "0049ed7f_abf6_48d9_84dc_49ea2de33f57",
+          name: "vendor_quotation_message_2_v1",
+          language: {
+            code: "en",
+          },
+          components: [
+            {
+              type: "body",
+              parameters: [
+                { type: "text", text: vendor_name},
+                { type: "text", text: quotationLink },
+              ],
+            },
+          ],
+        },
+      },
+      { headers }
+    );
+
+    return messageResponse.data;
+  } catch (error) {
+    console.error("Error sending vendor quotation message:", error.response?.data || error.message);
+    throw error;
+  }
+}
+
 export {
   sendInvoiceToWhatsApp,
   sendConfirmationMessageToWhatsapp,
@@ -579,5 +622,6 @@ export {
   handlePromoResponse,
   getVendors,
   sendVendorEventBookingMessage,
-  sendCustomerEventBookingMessage
+  sendCustomerEventBookingMessage,
+  sendVendorQuotationMessage
 };
