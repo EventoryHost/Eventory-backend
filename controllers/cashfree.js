61948e14f6da9f11787e87435e8f956f18de6ef4
Kartavya Rupal <kartavyarupal.kr@gmail.com>
2025-09-11 22:57:22 +0530

    added type to sendInvoice sqs message

--- controllers/cashfree.js
+++ controllers/cashfree.js
@@ -127,6 +127,7 @@ async function sendInvoice(req, res) {
 
     const vendor = await Vendor.findOne({ id: ven_id });
     const sqsMessage = {
+      type: "vendorOnboarded",
       customer: vendor,
       paymentDetails: formattedDetails,
     };
