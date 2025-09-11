df96ec2785ba57cae91db33850287f7c392ea6ff
Kartavya Rupal <kartavyarupal.kr@gmail.com>
2025-09-11 16:18:20 +0530

    added type to vendor onboarding sqs message

--- controllers/cashfree.js
+++ controllers/cashfree.js
@@ -91,6 +91,7 @@ const verifyPayment = async (req, res) => {
 
     const vendor = await Vendor.findOne({ id: ven_id });
     const sqsMessage = {
+      type: "vendorOnboarded",
       customer: vendor,
       paymentDetails: formattedDetails,
     };
