35c3bae9bcf2843819396105f586d48066027f48
Prasannakbhat123 <prasannakbhat123@users.noreply.github.com>
2025-10-24 14:08:46 +0530

    final order controller update

--- routes2/finalOrders.js
+++ routes2/finalOrders.js
@@ -9,6 +9,7 @@ import {
   getOrdersByCustomer,
   getOrderById,
   deleteFinalOrder,
+  updatePaymentDetails,
 } from "../controllers2/finalOrderController.js";
 
 const router = express.Router();
@@ -18,6 +19,7 @@ router.get("/finalOrder", getAllFinalOrders);
 router.get("/finalOrder/byQuotationId/:quotation_id", getOrderByQuotationId);
 router.put("/finalOrder/approve", approveFinalOrder);
 router.put("/finalOrder/:order_id", updateFinalOrder);
+router.put("/finalOrder/:order_id/payment", updatePaymentDetails);
 router.get("/finalOrder/vendor/:vendor_id", getOrdersByVendor);
 router.get("/finalOrder/customer/:customerId", getOrdersByCustomer);
 router.get("/finalOrder/:order_id", getOrderById);
