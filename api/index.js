f8431dfe998dc5891047a1f9f934322c2ef0adce
Prasannakbhat123 <prasannakbhat123@users.noreply.github.com>
2025-10-26 17:42:07 +0530

    Revert "spookygala"
    This reverts commit 3f03045b0dcebaac45c441bd1fd0b4109cfb0a35.

--- api/index.js
+++ api/index.js
@@ -39,7 +39,6 @@ import finalOrders from "../routes/finalOrders.js";
 import agreementRoutes from "../routes/agreementRoutes.js";
 import couponRoutes from "../routes/couponRoutes.js"; 
 import mediaRoutes from "../routes/mediaRoutes.js";
-import spookyGalaRoutes from "../routes/spookyGalaRoutes.js";
 
 const app = express();
 const port = process.env.PORT;
@@ -122,7 +121,6 @@ app.use("/api", finalOrders);
 app.use("/api/agreements", agreementRoutes);
 app.use("/api/coupons", couponRoutes); 
 app.use("/api/media", mediaRoutes);
-app.use("/api/spooky-gala", spookyGalaRoutes);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
