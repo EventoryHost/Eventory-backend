8e6db2bbeaa73f2f9cea332916d4baf44a9be375
Sumit Kumar <sumit182003@gmail.com>
2025-11-21 18:35:51 +0530

    added logs for env keys to check if any missing

--- api/index2.js
+++ api/index2.js
@@ -19,6 +19,9 @@ import decorator from "../routes2/reduxRoutes/decorator.js";
 dotenv.config();
 
 const app = express();
+console.log("🔍 Loaded ENV KEYS:");
+console.log(JSON.stringify(process.env, null, 2));
+
 const port = process.env.PORT || 4001; // Use different port for migration testing
 const router = Router();
 
