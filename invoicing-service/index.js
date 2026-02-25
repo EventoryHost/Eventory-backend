d11f180f762e77ade0bfa586739dcf2165c424c0
Ankit Kumar Chakraborty <ankitchakraborty1972@gmail.com>
2026-02-25 20:07:24 +0530

    Feature/item289 (#306)
    * update branch
    
    * handled changes in order model
    
    * Refining Checkout and Order Logic
    
    * non mandate email
    
    * bug fix

--- invoicing-service/index.js
+++ invoicing-service/index.js
@@ -13,7 +13,7 @@ const sqs = new SQSClient({
   },
 });
 
-const queueUrl = process.env.IS_DEV ? "https://sqs.ap-south-1.amazonaws.com/637423195802/invoice-test-queue" :
+const queueUrl = process.env.IS_DEV === "true" ? "https://sqs.ap-south-1.amazonaws.com/637423195802/invoice-test-queue" :
   "https://sqs.ap-south-1.amazonaws.com/637423195802/invoice-queue";
 
 console.log("Starting SQS polling service for invoicing and agreements...");
